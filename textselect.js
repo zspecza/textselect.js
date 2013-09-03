4ab9ebbad80690d644ba1921fcb120457f68543c
Declan de Wet <declandewet@me.com>
2013-09-03 14:04:59 +0200

    Added function that determines selection state and triggers  event

--- textselect.js
+++ textselect.js
@@ -55,12 +55,67 @@
       return function() {
         return window.getSelection().getRangeAt(0).getBoundingClientRect();
       };
-    } else {
-      return function() {
-        var message = "Selection coordinates are not supported in this browser";
-        throw new Error(message);
-      };
     }
   }).apply(this);
+
+  // the below 3 variables are used to prevent triggering the event
+  // when selected text is deselected on mouse click, as well as to
+  // ensure that text is actually selected before triggering the event
+
+  // stores selection text content
+  var selectionText = '',
+  // used to determine active state of selection
+  isTextSelected = false,
+  // used to ensure current selection content is not same as the previous one
+  previousSelection;
+
+  /**
+   * @function triggerSelectedTextEvent
+   * @param  {Object} $el [a jQuery element object]
+   * @returns nothing
+   *
+   * Determines current state of the selection and triggers the `textselect`
+   * event on the element passed to it
+   */
+  var triggerSelectedTextEvent = function($el) {
+
+    // get the selection text
+    selectionText = getSelectedTextContent();
+
+    // store the selection coords/size in this
+    var selectionInformation;
+
+    // sugary syntax for easy reading
+    var selectionNotEmpty = (selectionText !== ''),
+        uniqueSelection = (selectionText !== previousSelection);
+
+    // we do not want to trigger the `textselect` event if the selection
+    // is empty, if the user is clicking off a selection, or if the user
+    // is clicking on a selection to unselect it
+    if (selectionNotEmpty && uniqueSelection) {
+
+      // get the coordinates and size of the selection
+      selectionInformation = getSelectionInformation();
+
+      // trigger the event
+      $el.trigger('textselect', {
+        top: selectionInformation.top,
+        bottom: selectionInformation.bottom,
+        left: selectionInformation.left,
+        right: selectionInformation.right,
+        width: selectionInformation.width,
+        height: selectionInformation.height,
+        text: selectionText
+      });
+
+    }
+
+    // store the most recent selection for the next check
+    previousSelection = selectionText;
+
+    // reset selection content
+    selectionText = '';
+
+  };
 
 }).call(this);
