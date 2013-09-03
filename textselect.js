379888e8848ccef9a9ee093d53f7be35689782a6
Declan de Wet <declandewet@me.com>
2013-09-03 13:16:51 +0200

    Add function to fetch size and location of selection

--- textselect.js
+++ textselect.js
@@ -37,4 +37,30 @@
     }
   }).apply(this);
 
+  /**
+   * @function getSelectionInformation
+   * @return {Object} Contains coordinate and size properties of selection.
+   *
+   * Immediately-invoked self-modifying function.
+   * Executes instantly and uses feature detection to determine the
+   * method to use to get location and size information about the current
+   * selection, if any - then modifes itself to return the output of
+   * that method.
+   *
+   * N.B. - Temporary implementation until more research is done on
+   * supporting more browsers.
+   */
+  var getSelectionInformation = (function(){
+    if (window.getSelection) {
+      return function() {
+        return window.getSelection().getRangeAt(0).getBoundingClientRect();
+      };
+    } else {
+      return function() {
+        var message = "Selection coordinates are not supported in this browser";
+        throw new Error(message);
+      };
+    }
+  }).apply(this);
+
 }).call(this);
